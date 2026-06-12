457cc02dcd5f5f039af5e981b20ebbdee9e91eae
AdrielleLouise <adriellealrb@gmail.com>
2026-06-12 08:43:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -31,96 +31,45 @@ st.markdown("""
 - Confortável
 """)
 # variáveis de entrada
-temperatura = ctrl.Antecedent(
-    np.arange(0, 41, 1),
-    'temperatura')
-umidade = ctrl.Antecedent(
-    np.arange(0, 101, 1),
-    'umidade')
+temperatura = ctrl.Antecedent(np.arange(0, 41, 1), 'temperatura')
+umidade = ctrl.Antecedent(np.arange(0, 101, 1), 'umidade')
 
 #variável de saída
-conforto = ctrl.Consequent(
-    np.arange(0, 101, 1),
-    'conforto')
+conforto = ctrl.Consequent(np.arange(0, 101, 1), 'conforto')
 
 #Funções de Pertinência
 st.header("Funções de Pertinência")
-temperatura['frio'] = fuzz.trimf(
-    temperatura.universe,
-    [0, 0, 20])
-temperatura['agradavel'] = fuzz.trimf(
-    temperatura.universe,
-    [10, 20, 30])
-temperatura['quente'] = fuzz.trimf(
-    temperatura.universe,
-    [20, 40, 40])
+temperatura['frio'] = fuzz.trimf(temperatura.universe, [0, 0, 20])
+temperatura['agradavel'] = fuzz.trimf(temperatura.universe, [10, 20, 30])
+temperatura['quente'] = fuzz.trimf(temperatura.universe, [20, 40, 40])
 fig, ax = plt.subplots()
-ax.plot(
-    temperatura.universe,
-    temperatura['frio'].mf,
-    label='Frio')
-ax.plot(
-    temperatura.universe,
-    temperatura['agradavel'].mf,
-    label='Agradável')
-ax.plot(
-    temperatura.universe,
-    temperatura['quente'].mf,
-    label='Quente')
+ax.plot(temperatura.universe, temperatura['frio'].mf, label='Frio')
+ax.plot(temperatura.universe, temperatura['agradavel'].mf, label='Agradável')
+ax.plot(temperatura.universe, temperatura['quente'].mf, label='Quente')
 ax.set_title("Temperatura")
 ax.legend()
 st.pyplot(fig)
 
 #umidade
-umidade['baixa'] = fuzz.trimf(
-    umidade.universe,
-    [0, 0, 50])
-umidade['media'] = fuzz.trimf(
-    umidade.universe,
-    [0, 50, 100])
-umidade['alta'] = fuzz.trimf(
-    umidade.universe,
-    [50, 100, 100])
+umidade['baixa'] = fuzz.trimf(umidade.universe, [0, 0, 50])
+umidade['media'] = fuzz.trimf(umidade.universe, [0, 50, 100])
+umidade['alta'] = fuzz.trimf(umidade.universe, [50, 100, 100])
 fig, ax = plt.subplots()
-ax.plot(
-    umidade.universe,
-    umidade['baixa'].mf,
-    label='Baixa')
-ax.plot(
-    umidade.universe,
-    umidade['media'].mf,
-    label='Média')
-ax.plot(
-    umidade.universe,
-    umidade['alta'].mf,
-    label='Alta')
+ax.plot(umidade.universe, umidade['baixa'].mf, label='Baixa')
+ax.plot(umidade.universe, umidade['media'].mf, label='Média')
+ax.plot(umidade.universe, umidade['alta'].mf, label='Alta')
 ax.set_title("Umidade")
 ax.legend()
 st.pyplot(fig)
 
 #conforto e desconforto térmico
-conforto['desconfortavel'] = fuzz.trimf(
-    conforto.universe,
-    [0, 0, 50])
-conforto['moderado'] = fuzz.trimf(
-    conforto.universe,
-    [30, 50, 70])
-conforto['confortavel'] = fuzz.trimf(
-    conforto.universe,
-    [50, 100, 100])
+conforto['desconfortavel'] = fuzz.trimf(conforto.universe, [0, 0, 50])
+conforto['moderado'] = fuzz.trimf(conforto.universe, [30, 50, 70])
+conforto['confortavel'] = fuzz.trimf(conforto.universe, [50, 100, 100])
 fig, ax = plt.subplots()
-ax.plot(
-    conforto.universe,
-    conforto['desconfortavel'].mf,
-    label='Desconfortável')
-ax.plot(
-    conforto.universe,
-    conforto['moderado'].mf,
-    label='Moderado')
-ax.plot(
-    conforto.universe,
-    conforto['confortavel'].mf,
-    label='Confortável')
+ax.plot(conforto.universe, conforto['desconfortavel'].mf, label='Desconfortável')
+ax.plot(conforto.universe, conforto['moderado'].mf, label='Moderado')
+ax.plot(conforto.universe, conforto['confortavel'].mf, label='Confortável')
 ax.set_title("Conforto")
 ax.legend()
 st.pyplot(fig)
@@ -221,12 +170,9 @@ def calcular_conforto(temp, umi):
 caso1 = calcular_conforto(22, 40)
 caso2 = calcular_conforto(35, 25)
 st.header("Casos Solicitados pelo Professor Jorge")
-df = pd.DataFrame({
-    "Temperatura (°C)": [22, 35],
-    "Umidade (%)": [40, 25],
-    "Conforto": [
-        round(caso1, 2),
-        round(caso2, 2)
+df = pd.DataFrame({"Temperatura (°C)": [22, 35], "Umidade (%)": [40, 25], "Conforto": 
+    [
+        round(caso1, 2), round(caso2, 2)
     ]
 })
 st.table(df)
